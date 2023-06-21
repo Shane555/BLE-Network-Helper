10fa16cbae6193303e748543ed1ad25658726d32
shane <seeyx97@gmail.com>
2023-06-21 10:51:02 +0800

    Updated sensor pay load to 8 bytes as a generic formalisation of our inhouse protocol

--- includes/my_custom_models_def.h
+++ includes/my_custom_models_def.h
@@ -16,6 +16,7 @@
 #define ESP_BLE_MESH_VND_MODEL_OP_SET_UNACK ESP_BLE_MESH_MODEL_OP_3(0x0D, CID_ESP)
 #define ESP_BLE_MESH_VND_MODEL_OP_INTR_STATUS ESP_BLE_MESH_MODEL_OP_3(0x0E, CID_ESP)
 #define ESP_BLE_MESH_VND_MODEL_OP_DUMMY_SET ESP_BLE_MESH_MODEL_OP_3(0x0F, CID_ESP)
+#define ESP_BLE_MESH_VND_MODEL_OP_HEALTH_STATUS SENSOR_PROV_OP_3(0x0C, CID_ESP) //health status of sensors
 
 #define PROV_MODE_SET BACKEND_PROV_OP_3(0x00, CID_ESP)
 #define PROV_MODE_GET BACKEND_PROV_OP_3(0x01, CID_ESP)
@@ -28,7 +29,7 @@
 #define MESH_CLIENT_TIMEOUT BACKEND_PROV_OP_3(0x08, CID_ESP)
 #define PROV_NODE_INFO BACKEND_PROV_OP_3(0x0F, CID_ESP)
 
-#define SENSOR_PAYLOAD_BYTES 5
+#define SENSOR_PAYLOAD_BYTES 8
 #define MSG_C_BYTES (SENSOR_PAYLOAD_BYTES+5)
 
 #define MSG_A_BYTES (MSG_C_BYTES +2) //Msg A sensor format bytes
@@ -46,12 +47,16 @@
 
 typedef struct __attribute__((packed))
 {
-  uint8_t byte0;
+  uint8_t sensor_header;
   uint8_t byte1;
   uint8_t byte2;
   uint8_t byte3;
   uint8_t byte4;
+  uint8_t byte5;
+  uint8_t byte6;
+  uint8_t byte7;
 } model_sensor_data_t;
+
 
 typedef struct {
   uint32_t prov_payload;
